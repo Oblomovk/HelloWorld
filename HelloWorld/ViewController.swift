e4b9d3b637c5e13ed551590e27292fcc02ac259f
Konstantin Gonchaarov <goinmymind@gmail.com>
2024-12-07 23:19:50 +0300

    work with logic

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var greatingLabel: UILabel!
+    @IBOutlet var greatingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        greatingLabel.isHidden.toggle()
+        greatingButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func greatingButtonDidtapped(_ sender: UIButton) {
+        greatingLabel.isHidden.toggle()
+        sender.setTitle(
+            greatingLabel.isHidden ?"Show Greating" : "Hide Greating",
+            for: .normal
+        )
+    }
 }
-
